fba83d325f04a9e39ecc98951cdc75f66020bddf
innabatalova <batalovainna97@gmail.com>
2024-05-22 17:46:27 +0300

    Update app.page to ssr and routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,81 +1,19 @@
-'use client'
+import { FC } from "react"
+import { FormControl, Typography } from "@mui/material"
 
-import { FC, useState } from "react"
-import {
-  FormControl,
-  InputAdornment,
-  TextField,
-  Typography
-} from "@mui/material"
-import SearchIcon from '@mui/icons-material/Search'
-import ClearIcon from "@mui/icons-material/Clear"
-
-import validation from "@/utils/validation"
+import SearchField from "@/components/SearchField/page"
 
 import styles from './App.module.css'
 
 const App: FC = () => {
-  const [showClearIcon, setShowClearIcon] = useState<string>("none")
-  const [clearField, setClearField] = useState<string>("")
-  const [valid, setValid] = useState<boolean>(true)
-  const [valueInput, setValueInput] = useState<string>("")
-
-  const changeSearchInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    validation(event) ? setValid(true) : setValid(false)
-    setShowClearIcon(event.target.value === "" ? "none" : "flex")
-    setClearField(event.target.value)
-    setValueInput(event.target.value)
-  }
-
-  const clickClearIcon = (): void => {
-    setValid(true)
-    setShowClearIcon("none")
-    setClearField("")
-  }
-
-  const clickSearchIcon = (): void => {
-    console.log('tester SSR')
-
-  }
-
   return (
     <div className={styles.App}>
       <div className={styles.Wrapper}>
         <Typography variant="h3" className={styles.Title}>ПОИСК ПО ИНН</Typography>
-        <FormControl className={styles.FormControl}>
-          <TextField className={styles.TextField}
-            size="medium"
-            error={!valid ? true : false}
-            helperText={valid ? '' : 'Укажите данные в числовом формате'}
-            variant="outlined"
-            placeholder="Введите ИНН организации"
-            value={clearField}
-            onChange={changeSearchInput}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment
-                  position="start"
-                  onClick={clickSearchIcon}>
-                  <SearchIcon sx={{ cursor: 'pointer' }} />
-                </InputAdornment>
-              ),
-              endAdornment: (
-                <InputAdornment
-                  position="end"
-                  style={{ display: showClearIcon }}
-                  onClick={clickClearIcon}
-                >
-                  <ClearIcon sx={{ cursor: 'pointer' }} />
-                </InputAdornment>
-              )
-            }}
-          />
-        </FormControl>
+        <SearchField />
       </div>
-
     </div>
-
-  );
-};
+  )
+}
 
 export default App
---
+++ components/SearchField/page.tsx
@@ -0,0 +1,71 @@
+'use client'
+
+import { FC, useState } from "react"
+import { useRouter } from 'next/navigation'
+
+import { Button, InputAdornment, TextField } from "@mui/material"
+import SearchIcon from '@mui/icons-material/Search'
+import ClearIcon from "@mui/icons-material/Clear"
+
+import validation from "@/utils/validation"
+
+import styles from './searchField.module.css'
+
+const SearchField: FC = () => {
+  const router = useRouter()
+  
+  const [showClearIcon, setShowClearIcon] = useState<string>("none")
+  const [clearField, setClearField] = useState<string>("")
+  const [valid, setValid] = useState<boolean>(true)
+  const [valueInput, setValueInput] = useState<string>("")
+
+  const changeSearchInput = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    validation(event) ? setValid(true) : setValid(false)
+    setShowClearIcon(event.target.value === "" ? "none" : "flex")
+    setClearField(event.target.value)
+    setValueInput(event.target.value)
+  }
+
+  const clickClearIcon = (): void => {
+    setValid(true)
+    setShowClearIcon("none")
+    setClearField("")
+  }
+
+  const clickSearchIcon = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault()
+    router.push('/info/' + valueInput)
+  }
+
+  return (
+    <form onSubmit={clickSearchIcon}>
+      <TextField className={styles.TextField}
+        size="medium"
+        error={!valid ? true : false}
+        helperText={valid ? '' : 'Укажите данные в числовом формате'}
+        variant="outlined"
+        placeholder="Введите ИНН организации"
+        value={clearField}
+        onChange={changeSearchInput}
+        InputProps={{
+          startAdornment: (
+            <Button type="submit">
+              <SearchIcon sx={{ cursor: 'pointer' }} />
+            </Button>
+          ),
+          endAdornment: (
+            <InputAdornment
+              position="end"
+              style={{ display: showClearIcon }}
+              onClick={clickClearIcon}
+            >
+              <ClearIcon sx={{ cursor: 'pointer' }} />
+            </InputAdornment>
+          )
+        }}
+      />
+    </form>
+  )
+}
+
+export default SearchField
